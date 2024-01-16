c5b111b1c90acd3b598570b3958362e83a920996
FirstName LastName <lastname_firstname@example.com>
2024-01-17 00:48:11 +0900

    Separate UserTodos into own component. p.201

--- src/DisplayText.tsx
+++ src/DisplayText.tsx
@@ -1,4 +1,5 @@
 import React, { useState, FC } from 'react';
+import UserTodos from "./UserTodos";
 
 interface DisplayTextProps {
     getUserFullname: (username: string) => Promise<string>
@@ -7,7 +8,7 @@ interface DisplayTextProps {
 const DisplayText: FC<DisplayTextProps> = ({ getUserFullname }) => {
     const [txt, setTxt] = useState("");
     const [msg, setMsg] = useState("");
-    const [todos, setTodos] = useState<Array<JSX.Element>>();
+    const [todoControl, setTodoControl] = useState<ReturnType<typeof UserTodos>>()
 
     const onChangeTxt = (e: React.ChangeEvent<HTMLInputElement>) => {
         setTxt(e.target.value);
@@ -15,34 +16,9 @@ const DisplayText: FC<DisplayTextProps> = ({ getUserFullname }) => {
 
     const onClickShowMsg = async (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
         e.preventDefault();
+        setTodoControl(null);
         setMsg(`Welcome to React testing, ${await getUserFullname(txt)}`);
-        setUserTodos();
-    }
-
-    const setUserTodos = async () => {
-        const userResponse = await fetch('http://jsonplaceholder.typicode.com/users')
-        if (userResponse.ok) {
-            const users = await userResponse.json()
-            //console.log('users', users)
-            const userByName = users.find((usr: any) => {
-                return usr.username.toLowerCase() === txt
-            })
-            console.log('user by username', userByName)
-
-            const todosResponse = await fetch('http://jsonplaceholder.typicode.com/todos')
-            if(todosResponse.ok) {
-                const todos = await todosResponse.json()
-                console.log('todos: ', todos)
-                const usersTodos = await todos.filter((todo: any) => {
-                    return todo.userId === userByName.id
-                })
-                const todoList = usersTodos.map((todo: any) => {
-                    return <li key={todo.id}>{todo.title}</li>
-                })
-                setTodos(todoList)
-                console.log('user todos', usersTodos)
-            }
-        }
+        setTodoControl(<UserTodos username={txt} />)
     }
 
     return (
@@ -51,7 +27,7 @@ const DisplayText: FC<DisplayTextProps> = ({ getUserFullname }) => {
             <div><input data-testid="user-input" value={txt} onChange={onChangeTxt} /></div>
             <div><button data-testid="input-submit" onClick={onClickShowMsg}>Show Message</button></div>
             <div><label data-testid="final-msg">{msg}</label></div>
-            <ul style={{marginTop: '1rem', listStyleType: 'none'}}>{todos}</ul>
+            {todoControl}
         </form>
     )
 }
