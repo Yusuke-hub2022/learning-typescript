50dec51acc3b1f2577dd1037eeeee34f2a514350
FirstName LastName <lastname_firstname@example.com>
2024-01-16 22:46:33 +0900

    Show todos. p.197

--- src/DisplayText.tsx
+++ src/DisplayText.tsx
@@ -7,6 +7,7 @@ interface DisplayTextProps {
 const DisplayText: FC<DisplayTextProps> = ({ getUserFullname }) => {
     const [txt, setTxt] = useState("");
     const [msg, setMsg] = useState("");
+    const [todos, setTodos] = useState<Array<JSX.Element>>();
 
     const onChangeTxt = (e: React.ChangeEvent<HTMLInputElement>) => {
         setTxt(e.target.value);
@@ -15,6 +16,33 @@ const DisplayText: FC<DisplayTextProps> = ({ getUserFullname }) => {
     const onClickShowMsg = async (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
         e.preventDefault();
         setMsg(`Welcome to React testing, ${await getUserFullname(txt)}`);
+        setUserTodos();
+    }
+
+    const setUserTodos = async () => {
+        const userResponse = await fetch('http://jsonplaceholder.typicode.com/users')
+        if (userResponse.ok) {
+            const users = await userResponse.json()
+            //console.log('users', users)
+            const userByName = users.find((usr: any) => {
+                return usr.username.toLowerCase() === txt
+            })
+            console.log('user by username', userByName)
+
+            const todosResponse = await fetch('http://jsonplaceholder.typicode.com/todos')
+            if(todosResponse.ok) {
+                const todos = await todosResponse.json()
+                console.log('todos: ', todos)
+                const usersTodos = await todos.filter((todo: any) => {
+                    return todo.userId === userByName.id
+                })
+                const todoList = usersTodos.map((todo: any) => {
+                    return <li key={todo.id}>{todo.title}</li>
+                })
+                setTodos(todoList)
+                console.log('user todos', usersTodos)
+            }
+        }
     }
 
     return (
@@ -23,6 +51,7 @@ const DisplayText: FC<DisplayTextProps> = ({ getUserFullname }) => {
             <div><input data-testid="user-input" value={txt} onChange={onChangeTxt} /></div>
             <div><button data-testid="input-submit" onClick={onClickShowMsg}>Show Message</button></div>
             <div><label data-testid="final-msg">{msg}</label></div>
+            <ul style={{marginTop: '1rem', listStyleType: 'none'}}>{todos}</ul>
         </form>
     )
 }
