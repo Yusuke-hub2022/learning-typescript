15a658996fe441486476e063e125966544e7f49b
FirstName LastName <lastname_firstname@example.com>
2024-01-17 01:29:45 +0900

    Create mock file of UserTodos. p.202

---
+++ src/UserTodos.test.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+
+// p.203
+// テストするにはUserTodosコンポーネントのリファクタリングが必要だと思うので、
+// 飛ばして先へ進む
+describe("Test UserTodos", () => {
+    it("", () => {
+        //
+    })
+})
